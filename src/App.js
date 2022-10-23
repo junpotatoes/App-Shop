64eb0a49522b7f6115ee9b6af19cf22946a26da8
junpotatoes <mukdol1127@naver.com>
2022-10-23 16:13:16 +0900

    [Docs] Component 분리

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import { useState } from "react";
 import data from "./data.js";
+import Card from "./component/card.jsx";
 
 function App() {
   let [shoes] = useState(data);
@@ -23,36 +24,9 @@ function App() {
       <div div className="main-bg"></div>
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img
-              src="https://codingapple1.github.io/shop/shoes1.jpg
-
-"
-              width="80%"
-            />
-            <h4>{shoes[0].title}</h4>
-            <p>{shoes[0].price}</p>
-          </div>
-          <div className="col-md-4">
-            <img
-              src="https://codingapple1.github.io/shop/shoes2.jpg
-
-"
-              width="80%"
-            />
-            <h4>{shoes[1].title}</h4>
-            <p>{shoes[1].price}</p>
-          </div>
-          <div className="col-md-4">
-            <img
-              src="https://codingapple1.github.io/shop/shoes3.jpg
-
-"
-              width="80%"
-            />
-            <h4>{shoes[2].title}</h4>
-            <p>{shoes[2].price}</p>
-          </div>
+          {shoes.map((el, i) => {
+            return <Card shoes={shoes[i]} i={i + 1}></Card>;
+          })}
         </div>
       </div>
     </div>
